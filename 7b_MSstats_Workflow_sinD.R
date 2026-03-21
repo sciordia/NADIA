c314e975265b4ec467c26234629d2d30bbe2c619
Sergio Ciordia <sciordia@cnb.csic.es>
2026-03-21 15:18:06 +0100

    Updated files

---
+++ 7b_MSstats_Workflow_sinD.R
@@ -0,0 +1,36 @@
+
+# MSstats Pipeline ("noBIG")
+raw <- read.csv("./data/20260306_CursoProtQ_2024_DIA_Exploris_v20p5_sinImp_sinNorm_sinD_MSstats.tsv", sep = "\t")
+annotation <-  read.csv("./data/MSstats_Annotation_sinD.csv")
+
+quant <-SpectronauttoMSstatsFormat(raw,
+                                   annotation = annotation,
+                                   intensity = "PeakArea",
+                                   filter_with_Qvalue = TRUE,
+                                   qvalue_cutoff = 0.01,
+                                   useUniquePeptide = TRUE,
+                                   removeFewMeasurements = TRUE,
+                                   removeProtein_with1Feature = FALSE,
+                                   summaryforMultipleRows = max
+                                   )
+
+
+
+processed.quant <- dataProcess(quant, normalization = "equalizeMedians")
+
+# Define the comparison matrix
+comparison <- matrix(c(
+  -1,  1,  0,   # B - A
+  -1,  0,  1,   # C - A
+  0, -1,  1   # C - B
+), ncol = 3, byrow = TRUE)
+row.names(comparison) <- c("B-A", "C-A", "C-B")
+colnames(comparison) <- c("A", "B", "C")
+
+comparison_result <- groupComparison(contrast.matrix = comparison,
+                                     data = processed.quant
+                                     )
+
+# Save DF
+DEPs_results <- comparison_result$ComparisonResult
+readr::write_tsv(DEPs_results, file = "./results/Q24_DIA_Spectronaut_20p5_MSstats_sinD//DEPs_results_MSstats_sinD.tsv")
